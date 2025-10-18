d96871854ef08c0bc1f828ed8cc75576e1ff5ffd
MtuyetHTTT 18-01 <lyminhtuyet1110@gmail.com>
2025-10-18 12:26:34 +0700

    Add files via upload

---
+++ Mảng/TimChiSoMaxCuoiCung.java
@@ -0,0 +1,31 @@
+package Mảng;
+
+import java.util.Scanner;
+
+public class TimChiSoMaxCuoiCung {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng phần tử N: ");
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        int giaTriMax = a[0];
+        int chiSoMax = 0;
+        for (int i = 1; i < n; i++) {
+            if (a[i] >= giaTriMax) {
+                giaTriMax = a[i];
+                chiSoMax = i;
+            }
+        }
+
+        System.out.println("Chỉ số của phần tử lớn nhất cuối cùng là: " + chiSoMax);
+
+        scanner.close();
+    }
+}
---
+++ Mảng/TinhTongTheoDieuKien.java
@@ -0,0 +1,29 @@
+package Mảng;
+
+import java.util.Scanner;
+
+public class TinhTongTheoDieuKien {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng phần tử N: ");
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        long tong = 0;
+
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] % 2 == 0 && i % 2 != 0) {
+                tong += a[i];
+            }
+        }
+
+        System.out.println("Tổng các phần tử chẵn ở chỉ số lẻ là: " + tong);
+
+        scanner.close();
+    }
+}
